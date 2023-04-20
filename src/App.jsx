0e2e1f6bc14a02b6d42e2cf8bdc32c3d69b1ee9b
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-04-20 11:18:46 -0400

    create the about component and added stayles and content to it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Home from './components/Home';
 import SocialLinks from './components/SocialLinks';
+import About from './components/About';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div>
       <NavBar />
       <Home />
+      <About />
 
       <SocialLinks />
     </div>
