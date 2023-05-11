8b29ae21907c14c563c2e0ab7176f747f1171ef1
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-05-11 18:21:34 -0400

    added to experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -75,7 +75,7 @@ function Experience() {
   return (
     <div name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
 
-      <div className="max-w-screen-lg mx-auto p-4 pt-8 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 pt-11rem flex flex-col justify-center w-full h-full text-white">
 
         <div>
 
