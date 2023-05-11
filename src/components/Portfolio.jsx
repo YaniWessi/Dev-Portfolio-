976a4fa8eb8280afcc1ba5e9b434808b0dc07eb3
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-05-11 18:36:07 -0400

    added something

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -39,7 +39,7 @@ const Portfolio = () => {
   ]
   return (
     <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 pt-44 mx-auto flex flex-col justify-center w-full h-full">
           <div className="pb-8">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
             <p className="py-6">Coming soon!</p>
