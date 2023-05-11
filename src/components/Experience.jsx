31b6e0422f06b1a151a688ba3124adc983774c75
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-05-11 17:40:55 -0400

    changed my resume link to an updated version

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -75,7 +75,7 @@ function Experience() {
   return (
     <div name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
 
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-10 flex flex-col justify-center w-full h-full text-white">
 
         <div>
 
