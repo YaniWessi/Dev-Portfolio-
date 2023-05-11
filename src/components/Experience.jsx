439278ad553e79302817c48d01ce5573838ec742
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-05-11 18:32:46 -0400

    added something

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -75,11 +75,11 @@ function Experience() {
   return (
     <div name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
 
-      <div className="max-w-screen-lg mx-auto p-4 pt-45 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 pt-8 flex flex-col justify-center w-full h-full text-white">
 
         <div>
 
-          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
+          <p className="text-4xl font-bold border-b-44 border-gray-500 p-2 inline">Experience</p>
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
