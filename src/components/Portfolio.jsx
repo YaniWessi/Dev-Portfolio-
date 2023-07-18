40117303cc61f3c57a5c64c467133819e2275b44
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-07-18 19:30:09 -0400

    dashboard project github

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -37,6 +37,12 @@ const Portfolio = () => {
     },
    
   ]
+
+
+  const handDemoClick = () => {
+        window.location.href = 'https://github.com/YaniWessi/AdminDashboard.git'
+  }
+
   return (
     <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
       <div className="max-w-screen-lg p-4 pt-44 mx-auto flex flex-col justify-center w-full h-full">
@@ -51,7 +57,7 @@ const Portfolio = () => {
                 <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                   <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
                   <div className="flex items-center justify-center">
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={handDemoClick}>Demo</button>
                     <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
                   </div>
                 </div>
