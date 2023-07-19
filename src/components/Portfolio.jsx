285424d5719e9c0a4c96e8aaae2b2939a5f21cc6
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-07-18 20:46:43 -0400

    space between

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -20,7 +20,7 @@ const Portfolio = () => {
   }
 
   return (
-    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full pb-30 text-white md:h-screen">
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full pb-96 text-white md:h-screen">
       <div className="max-w-screen-lg p-4 pt-44 mx-auto flex flex-col justify-center w-full h-full">
           <div className="pb-8">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
